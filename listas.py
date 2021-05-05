f94e6b3e1318a3cdaf4b717eb35cc1a17dcff53b
dzaton_ <dzatons01@educantabria.es>
2021-05-05 23:29:21 +0200

    listas

---
+++ listas.py
@@ -0,0 +1,18 @@
+#Las list son colecciones ordenadas e intercambiables y que permite elementos duplicados
+
+lista = ["manzana", "pera", "frambuesa"]
+print(lista)
+print("Longitud de la lista es de: ", len(lista))
+
+list2 = [1, 5, 3, 9]
+list3 = [True, False, False]
+list4 = ["string", 24, True, 32, "cosa"]
+
+print("Tipo de dato: ", type(list2))
+
+thislist = list(("manzana", "tetera", "camioneta")) # note the double round-brackets
+print(thislist)
+
+
+
+
