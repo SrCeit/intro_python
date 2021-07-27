9d3d299956ddb8075c3a056887ad08470fbfaf79
dzaton_ <dzatons01@educantabria.es>
2021-07-28 01:22:54 +0200

    ejemplos de funciones

---
+++ 5_programacion_estructurada/ejemplos/ejemplo3.py
@@ -0,0 +1,27 @@
+def calcular_segundos(horas,minutos,segundos):
+    return horas*3600+minutos*60+segundos    
+
+def calcular_horas(segundos):
+    horas = segundos // 3600
+    segundos-=horas*3600
+    minutos = segundos // 60
+    segundos-=minutos*60
+    return horas,minutos,segundos
+
+def calcular(*args):
+    if len(args)==1:
+        return calcular_horas(args[0])
+    elif len(args)==3:
+        return calcular_segundos(*args)
+    else:
+        raise TypeError("Se espera 1 o 3 parámetros")
+
+if __name__ == '__main__':
+    print(calcular(4324))
+    print(calcular(3,45,43))
+    
+    hora = map(str,calcular_horas(12296))
+    print(":".join(hora))
+
+
+
